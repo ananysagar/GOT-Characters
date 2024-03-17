6c293f1d845a73fd221b84be6dc658d852db9b4f
Anany Sagar <sagaranany577@gmail.com>
2024-03-17 23:23:03 +0530

    Got character app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,75 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react'
+import axios from 'axios';
 import './App.css';
+import Header from './components/Header';
+import CharacterGrid from './components/CharacterGrid';
+import Search from './components/Search'
 
-function App() {
+const App = () => {
+  const [items, setItems] = useState([])
+  const [isLoading, setIsLoading] = useState(true)
+  const [query, setQuery] = useState('')
+
+  useEffect(() => {
+    const fetchItems = async () => {
+      setIsLoading(true)
+      const result = await axios(
+        `https://thronesapi.com/api/v2/Characters?title=${query}`
+      )
+
+      //console.log(result.data)
+
+      setItems(result.data)
+      setIsLoading(false)
+    }
+
+    fetchItems()
+  }, [query])
+  
+  const queryFunction = (q) =>{
+    setQuery(q)
+  }
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <Header />
+      <Search getQuery={queryFunction} />
+      <CharacterGrid isLoading={isLoading} items={items} />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
+
+// function App() {
+//   const [items, setItems] = useState([])
+//   const [isLoading, setisLoading] = useState(true)
+//   const [query, setQuery] = useState('')
+
+//   useEffect(() => {
+//     const fetchItems = async () => {
+//       setisLoading(true)
+//       const result = await axios(
+//         `https://thronesapi.com/api/v2/Characters?fullName=${query}`
+//         )
+//       setItems(result.data)
+//       setisLoading(false)
+//     }
+
+//     fetchItems()
+//   }, [query])
+
+//   const queryFunction = (q) =>{
+//     setQuery(q)
+//   }
+
+//   return (
+//     <div className='container'>
+//       <Header />
+//       <Search getQuery={queryFunction} />
+//       <CharacterGrid isLoading={isLoading} items={items} />
+//     </div>
+//   )
+// }
+
+// export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Logo from '../img/logo.png'
+
+export default function Header() {
+  return (
+    <header className='center'>
+       <img src={Logo} alt='' />
+    </header>
+  )
+}
