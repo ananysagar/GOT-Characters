73f104175127f20c615d2bb1f5188c33233da8ee
ananysagar <sagaranany577@gmail.com>
2024-04-27 00:40:51 +0530

    quick fix

--- src/components/CharacterGrid.js
+++ src/components/CharacterGrid.js
@@ -9,7 +9,6 @@ const CharacterGrid = ( { items, isLoading }) => {
   <section className='cards'>
     {items.map(item =>(
         <CharacterItem key={item.id} item={item}></CharacterItem>
-        
     ))}
   </section>
   )
